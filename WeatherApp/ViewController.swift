3b53e933e43fdb46f77bab99b6ef43747c33eb52
Lasat17 <lasat17@student.sdu.dk>
2020-10-21 16:54:08 +0200

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Lavan Sathiyaseelan on 21/10/2020.
+//  Copyright © 2020 WeatherApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
