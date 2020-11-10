5d6797a9978b2fdf78546b9bf0e9e4a52eebd27a
Julie Dittmann Weimar Andersen <smukkejul@live.dk>
2020-11-10 15:50:28 +0100

    TableView Workin on addCity

---
+++ WeatherApp/Model/API/URLBase.swift
@@ -0,0 +1,36 @@
+//
+//  APIBaseKey.swift
+//  WeatherApp
+//
+//  Created by Julie Dittmann Weimar Andersen on 09/11/2020.
+//  Copyright © 2020 WeatherApp. All rights reserved.
+//
+
+import Foundation
+
+struct URLBase {
+    
+    //"https://api.openweathermap.org/data/2.5/find?units=metric&q=london&appid=300e9251ab98a3f16bd8091afbaeb5eb"
+    private let base_url = "https://api.openweathermap.org/data/2.5/"
+    private let units_url = "?units=metric"
+    private let url_Key = "&appid=300e9251ab98a3f16bd8091afbaeb5eb"
+    private let url_find = "find"
+    private let url_forcast = "forecast"
+    private let url_weatherList = "group"
+    
+    func findCityUrl(for city: String) -> String{
+        return "\(base_url)\(url_find)\(units_url)&q=\(city)\(url_Key)"
+    }
+    
+    func forcastUrl(id city: String) -> String{
+        return "\(base_url)\(url_forcast)\(units_url)&id=\(city)\(url_Key)"
+    }
+    
+    func weatherListUrl(id cities: [String]) -> String{
+        var idlist = ""
+        for cityId in cities{
+            idlist += "\(cityId),"
+        }
+        return "\(base_url)\(url_weatherList)\(units_url)&id=\(idlist)\(url_Key)"
+    }
+}
