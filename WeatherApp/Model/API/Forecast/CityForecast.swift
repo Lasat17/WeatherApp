22a808ecafe655e8a14debb42484701c9851ea9d
Julie Dittmann Weimar Andersen <smukkejul@live.dk>
2020-11-11 10:32:15 +0100

    Added Forecast and WeaterList
    add structs for json decoder to use When the api is called for forecast of a city and the list of current weather of a list of cities

---
+++ WeatherApp/Model/API/Forecast/CityForecast.swift
@@ -0,0 +1,124 @@
+//
+//  Forecast.swift
+//  WeatherApp
+//
+//  Created by Julie Dittmann Weimar Andersen on 11/11/2020.
+//  Copyright © 2020 WeatherApp. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Forecast
+struct CityForecast: Codable {
+    let cod: String
+    let message, cnt: Int
+    let list: [List]
+    let city: CityOfForecast
+}
+
+// MARK: - City
+struct CityOfForecast: Codable {
+    let id: Int
+    let name: String
+    let coord: Coord
+    let country: String
+    let population, timezone, sunrise, sunset: Int
+}
+
+// MARK: - Coord
+struct Coord: Codable {
+    let lat, lon: Double
+}
+
+// MARK: - List
+struct List: Codable {
+    let dt: Int
+    let main: MainClass
+    let weather: [Weather]
+    let clouds: Clouds
+    let wind: Wind
+    let visibility: Int
+    let pop: Double
+    let sys: Sys
+    let dtTxt: String
+
+    enum CodingKeys: String, CodingKey {
+        case dt, main, weather, clouds, wind, visibility, pop, sys
+        case dtTxt = "dt_txt"
+    }
+}
+
+// MARK: - Clouds
+struct Clouds: Codable {
+    let all: Int
+}
+
+// MARK: - MainClass
+struct MainClass: Codable {
+    let temp, feelsLike, tempMin, tempMax: Double
+    let pressure, seaLevel, grndLevel, humidity: Int
+    let tempKf: Double
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure
+        case seaLevel = "sea_level"
+        case grndLevel = "grnd_level"
+        case humidity
+        case tempKf = "temp_kf"
+    }
+}
+
+// MARK: - Sys
+struct Sys: Codable {
+    let pod: Pod
+}
+
+enum Pod: String, Codable {
+    case d = "d"
+    case n = "n"
+}
+
+// MARK: - Weather
+struct Weather: Codable {
+    let id: Int
+    let main: MainEnum
+    let weatherDescription: Description
+    let icon: Icon
+
+    enum CodingKeys: String, CodingKey {
+        case id, main
+        case weatherDescription = "description"
+        case icon
+    }
+}
+
+enum Icon: String, Codable {
+    case the01D = "01d"
+    case the01N = "01n"
+    case the03D = "03d"
+    case the03N = "03n"
+    case the04D = "04d"
+    case the04N = "04n"
+}
+
+enum MainEnum: String, Codable {
+    case clear = "Clear"
+    case clouds = "Clouds"
+}
+
+enum Description: String, Codable {
+    case brokenClouds = "broken clouds"
+    case clearSky = "clear sky"
+    case overcastClouds = "overcast clouds"
+    case scatteredClouds = "scattered clouds"
+}
+
+// MARK: - Wind
+struct Wind: Codable {
+    let speed: Double
+    let deg: Int
+}
---
+++ WeatherApp/Model/API/WeatherList/WeatherList.swift
@@ -0,0 +1,75 @@
+//
+//  WeatherList.swift
+//  WeatherApp
+//
+//  Created by Julie Dittmann Weimar Andersen on 11/11/2020.
+//  Copyright © 2020 WeatherApp. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - WeatherList
+struct WeatherList: Codable {
+    let cnt: Int
+    let list: [ListWeatherList]
+}
+
+// MARK: - List
+struct ListWeatherList: Codable {
+    let coord: CoordWeatherList
+    let sys: SysWeatherList
+    let weather: [WeatherWeatherList]
+    let main: MainWeatherList
+    let visibility: Int
+    let wind: WindWeatherList
+    let clouds: CloudsWeatherList
+    let dt, id: Int
+    let name: String
+}
+
+// MARK: - Clouds
+struct CloudsWeatherList: Codable {
+    let all: Int
+}
+
+// MARK: - Coord
+struct CoordWeatherList: Codable {
+    let lon, lat: Double
+}
+
+// MARK: - Main
+struct MainWeatherList: Codable {
+    let temp, feelsLike, tempMin, tempMax: Double
+    let pressure, humidity: Int
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure, humidity
+    }
+}
+
+// MARK: - Sys
+struct SysWeatherList: Codable {
+    let country: String
+    let timezone, sunrise, sunset: Int
+}
+
+// MARK: - Weather
+struct WeatherWeatherList: Codable {
+    let id: Int
+    let main, weatherDescription, icon: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, main
+        case weatherDescription = "description"
+        case icon
+    }
+}
+
+// MARK: - Wind
+struct WindWeatherList: Codable {
+    let speed, deg: Int
+}
