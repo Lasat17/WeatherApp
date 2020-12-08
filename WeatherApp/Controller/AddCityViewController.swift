aa5f0815e47db365dc92c9e4f89380989c9a8038
Julie Dittmann Weimar Andersen <smukkejul@live.dk>
2020-12-08 13:59:50 +0100

    clean up addCityViewController
    Removed some code that was commented out and changed the error messages

--- WeatherApp/Controller/AddCityViewController.swift
+++ WeatherApp/Controller/AddCityViewController.swift
@@ -17,26 +17,21 @@ class AddCityViewController : UIViewController{
     let api = URLBase()
     
     var findCity : FindCity? = nil
-    
-    //database kode
+
     private var weatherAppDataModelManager: WeatherAppDataModelManager!
-    //private var forecasts: [NSManagedObject]?
-    
-    
+
     @IBOutlet weak var ActivityIndicator: UIActivityIndicatorView!
    
     override func viewWillAppear(_ animated: Bool) {
         weatherAppDataModelManager = WeatherAppDataModelManager()
-        //forecasts = weatherAppDataModelManager.getAllForecasts()
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.TableView.isHidden = true
         self.TableView.dataSource = self
         self.TableView.delegate = self
-        
         self.ActivityIndicator.isHidden = true
-        
     }
     
     @IBOutlet weak var SearchCityText: UITextField!
@@ -46,13 +41,7 @@ class AddCityViewController : UIViewController{
         ActivityIndicator.startAnimating()
         getCityData()
     }
-    
-/*
-    override func viewWillAppear(_ animated: Bool) {
-        weatherAppDataModelManager = WeatherAppDataModelManager()
-        forecasts = weatherAppDataModelManager.getAllForecasts()
-    }
-    */
+
     @IBOutlet weak var TableView: UITableView!
     
     //func getCityData(for city: String)
@@ -65,61 +54,44 @@ class AddCityViewController : UIViewController{
             let task = session.dataTask(with: request, completionHandler: {(data, response, error) in DispatchQueue.main.async {
                 
                 if error != nil{
-                    self.showError(description: "Something went wrong. Try again.")
-                    print("1")
+                    self.showError(description: "An error occurred")
                     return
                 }
                 
                 if let response1 = response as? HTTPURLResponse{
                     if response1.statusCode == 400 {
-                        self.showError(description: "Invalid currency. Try another.")
-                        print("2")
+                        self.showError(description: "Could not get the weather data")
                         return
                     }
                 }
                 
-              
                 if let data1 = data, let findCity1 = try? self.jsonDecoder.decode(FindCity.self, from: data1){
                     self.ActivityIndicator.isHidden = true
                     self.ActivityIndicator.stopAnimating()
                     self.TableView.isHidden =  false
                     self.findCity = findCity1
                     self.TableView.reloadData()
-                    print("hello")
                 } else {
-                    self.showError(description: "Something went wrong. Try again.")
-                    
-                    if let response1 = response as? HTTPURLResponse{
-                        print("\(response1.statusCode)")
-                        
-                    }
-
+                    self.showError(description: "Something went wrong")
                 }
-                
                 }})
-            
             task.resume()
         }
-        
     }
-    
     
     func showError(description: String){
         self.ActivityIndicator.isHidden = true
-        let alert = UIAlertController(title: "Error", message: description, preferredStyle: UIAlertController.Style.alert)
+        let alert = UIAlertController(title: "Error", message: "\(description).\n Please try again later ", preferredStyle: UIAlertController.Style.alert)
         alert.addAction(UIAlertAction(title:"Ok", style: UIAlertAction.Style.default, handler: {_ in self.navigationController?.popViewController(animated: true)}))
-        
+
         self.present(alert, animated: true, completion: nil)
     }
-
 }
-
 
 extension AddCityViewController : UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if self.findCity != nil {
-            print("nub of item")
             return (findCity?.list.count)!
         }
         return 0
@@ -132,26 +104,14 @@ extension AddCityViewController : UITableViewDataSource, UITableViewDelegate {
             let cityCountry = resultCities.list[indexPath.item].sys.country
             cell.AddCItyNameUILabel.text = "\(cityName), \(cityCountry)"
         }
-        
         return cell
     }
-    
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
         self.TableView.deselectRow(at: indexPath, animated: true)
         if !weatherAppDataModelManager.getCityID(cityID: "\((findCity?.list[indexPath.item].id)!)") {
-            //let forecast = (self.weatherAppDataModelManager?.addCity(cityID: "\((findCity?.list[indexPath.item].id)!)", cityName: findCity?.list[indexPath.item].name, country: findCity?.list[indexPath.item].sys.country))!
-       
             (self.weatherAppDataModelManager?.addCity(cityID: "\((findCity?.list[indexPath.item].id)!)", cityName: findCity?.list[indexPath.item].name, country: findCity?.list[indexPath.item].sys.country))!
-            
-             //self.forecasts!.append(forecast)
         }
-        
         self.navigationController?.popToRootViewController(animated: true)
-   
-        
-        
     }
-    
 }
-
